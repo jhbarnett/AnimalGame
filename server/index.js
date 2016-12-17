aff4a330da7a1a12dd3983c9517edb177d27fad6
Jason Barnett <jason@thebarnetts.ws>
2016-12-16 17:12:31 -0800

    basic scaffold & server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const Express = require('express')
+const Path = require('path')
+const BodyParser = require('body-parser')
+const App = Express()
+const Routes = require('./routes')
+
+const assetFolder = Path.resolve(__dirname, '../public')
+
+App.use(BodyParser.json())
+
+App.use(BodyParser.urlencoded({extended: true}))
+
+const port = process.env.PORT || 8080
+
+App.listen(port)
+
+console.log(`Listening on port ${port}...`)
