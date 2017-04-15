6fe4a97253fe07c83c73a8aae414edfa0ce5db41
Jason Barnett <jason@thebarnetts.ws>
2017-04-14 20:11:54 -0500

    configures create user view, serializer, and tests

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
-  
+
 from .models import Game
 from .models import Question
 from .serializers import GameSerializer
 from .serializers import QuestionSerializer
+
   
 class GameList(generics.ListCreateAPIView):
 	"""
@@ -38,4 +39,29 @@ class QuestionInstance(generics.RetrieveUpdateAPIView):
 	serializer_class = QuestionSerializer
 	def get_queryset(self):
 		id = self.kwargs['pk']
-		return Question.objects.filter(pk=id)	
+		return Question.objects.filter(pk=id)
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+
+class UserCreate(APIView):
+	""" 
+	Creates the user. 
+	"""
+	def post(self, request, format='json'):
+		serializer = UserSerializer(data=request.data)
+		if serializer.is_valid():
+			user = serializer.save()
+			if user:
+				token = Token.objects.create(user=user)
+				json = serializer.data
+				json['token'] = token.key
+				return Response(json, status=status.HTTP_201_CREATED)
+		
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)				
